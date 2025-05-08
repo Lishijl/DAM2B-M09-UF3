3f9a5ce3c3be9aa11f87fc3de3f027651d65598a
Lishi Ji Liu <liulishij@gmail.com>
2025-05-08 18:42:43 +0200

    03-Transferencia classe Fitxer completat

--- 03-Transferencia/fitxers/src/main/java/com/iticbcn/Client.java
+++ 03-Transferencia/fitxers/src/main/java/com/iticbcn/Client.java
@@ -1,10 +1,12 @@
 package com.iticbcn;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
     static final String DIR_ARRIBADA = System.getProperty("java.io.tmpdir");
@@ -16,7 +18,25 @@ public class Client {
         out = new ObjectOutputStream(socket.getOutputStream());
         in = new ObjectInputStream(socket.getInputStream());
     }
-    public void rebreFitxers() {}
-    public void tancarConnexio() {}
-    public static void main(String[] args) {}
+    // envia el fitxer llegit per consola amb ruta completa que s'envia al servidor per rebre byte[]
+    public void rebreFitxers() {
+        String nomFitxer = "";
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Introdueix el nom del fitxer a consultar amb ruta completa:");
+            String path = sc.nextLine();
+            nomFitxer = new File(path).getName();
+            // a la recepció si hi ha algo que llegir, o té contingut, escriu en el directori temporal
+        }
+    }
+    public void tancarConnexio() throws IOException {
+        if (out != null) out.close();
+        if (in != null) in.close();
+        if(socket != null) socket.close();
+    }
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Client cli = new Client();
+        cli.connectar();
+        cli.rebreFitxers();
+        cli.tancarConnexio();
+    }
 }
--- 03-Transferencia/fitxers/src/main/java/com/iticbcn/Fitxer.java
+++ 03-Transferencia/fitxers/src/main/java/com/iticbcn/Fitxer.java
@@ -1,5 +1,23 @@
 package com.iticbcn;
 
-public class Fitxer {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class Fitxer {
+    private final String nom;
+    private byte[] contingut;
+    public Fitxer(String nom) {
+        this.nom = nom;
+    }
+    public byte[] getContingut() throws IOException {
+        File fitxer = new File(nom);
+        contingut = null;
+        if (fitxer.exists() && fitxer.isFile()) {
+            Path path = fitxer.toPath();
+            contingut = Files.readAllBytes(path);
+        }
+        return contingut;
+    }
 }
