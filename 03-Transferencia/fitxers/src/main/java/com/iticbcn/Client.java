287eff65d7cd3f754ac6c4a19a51270b4fe80f6b
Lishi Ji Liu <liulishij@gmail.com>
2025-05-13 19:48:58 +0200

    03-Transferencia finalitzat

--- 03-Transferencia/fitxers/src/main/java/com/iticbcn/Client.java
+++ 03-Transferencia/fitxers/src/main/java/com/iticbcn/Client.java
@@ -1,11 +1,12 @@
 package com.iticbcn;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Client {
@@ -14,29 +15,46 @@ public class Client {
     private ObjectOutputStream out;
     private ObjectInputStream in;
     public void connectar() throws UnknownHostException, IOException {
+        System.out.println("\nConnectant a -> " + Servidor.HOST + ":" + Servidor.PORT);
         socket = new Socket(Servidor.HOST, Servidor.PORT);
+        System.out.println("Connexio acceptada: " + socket.getInetAddress());       // l'adreça del remot, servidor localhost/127.0.0.1
         out = new ObjectOutputStream(socket.getOutputStream());
         in = new ObjectInputStream(socket.getInputStream());
     }
-    // envia el fitxer llegit per consola amb ruta completa que s'envia al servidor per rebre byte[]
-    public void rebreFitxers() {
+    public void rebreFitxers() throws IOException, ClassNotFoundException {
         String nomFitxer = "";
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Introdueix el nom del fitxer a consultar amb ruta completa:");
+            System.out.print("Nom del fitxer a rebre ('sortir' per sortir): ");
+            while (true) { 
             String path = sc.nextLine();
-            nomFitxer = new File(path).getName();
-            // a la recepció si hi ha algo que llegir, o té contingut, escriu en el directori temporal
+            if (path.equalsIgnoreCase("sortir")) break;
+                nomFitxer = path;
+                out.writeObject(nomFitxer);
+                out.flush();
+                String desti = DIR_ARRIBADA + Paths.get(nomFitxer).getFileName().toString();
+                int tamany = in.readInt();
+                if (tamany > 0) {
+                    System.out.println("Nom del fitxer a guardar: " + desti);
+                    Object contingutObj = in.readObject();
+                    if (contingutObj instanceof byte[] contingutByte) {
+                        Files.write(Paths.get(DIR_ARRIBADA, Paths.get(nomFitxer).getFileName().toString()), contingutByte);
+                        System.out.println("Fitxer rebut i guardat com: " + desti);
+                    }
+                }
+            }
         }
     }
     public void tancarConnexio() throws IOException {
+        System.out.println("Sortint...");
         if (out != null) out.close();
         if (in != null) in.close();
-        if(socket != null) socket.close();
+        if (socket != null) socket.close();
     }
-    public static void main(String[] args) throws UnknownHostException, IOException {
+    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
         Client cli = new Client();
         cli.connectar();
         cli.rebreFitxers();
         cli.tancarConnexio();
+        System.out.println("Connexio tancada.");
     }
 }
--- 03-Transferencia/fitxers/src/main/java/com/iticbcn/Servidor.java
+++ 03-Transferencia/fitxers/src/main/java/com/iticbcn/Servidor.java
@@ -1,6 +1,9 @@
 package com.iticbcn;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,24 +11,55 @@ public class Servidor {
     static final int PORT = 9999;
     static final String HOST = "localhost";
     private ServerSocket srvSocket;
-    private Socket clientSocket;
-    // server escolta pel port i retorna el clientSocket acceptat
+    private Socket cliSocket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
     public Socket connectar() throws IOException {
+        System.out.println("\nAcceptant connexions en -> " + HOST + ":" + PORT);
         srvSocket = new ServerSocket(PORT);
+        System.out.println("Esperant connexio...");
         return srvSocket.accept();
     }
     // pot tenir multi connexó amb clients, però es tanca només el rebut i 
     // el servidor només tanca abans de sortir
     public void tancarConnexio(Socket cliSocket) throws IOException {
+        System.out.println("Tancant connexió amb el client: " + cliSocket.getLocalAddress());
         if (cliSocket != null) cliSocket.close();
     }
-    // reb nomFitxer per enviar al client - llegeix si té contingut
-    // conversió en byte[] i enviar per xarxa
-    public void enviarFitxers() {}
+    public void enviarFitxers() throws ClassNotFoundException, IOException {
+        System.out.println("Esperant el nom del fitxer del client...");
+        while (true) {
+            try {
+                String ruta = (String) in.readObject();
+                System.out.println("Nomfitxer rebut: " + ruta);
+                Fitxer fitxer = new Fitxer(ruta);
+                byte[] contingut = fitxer.getContingut();
+                if (contingut != null) {
+                    System.out.println("Contingut del fitxer a enviar: " + contingut.length + " bytes");
+                    out.writeInt(contingut.length);
+                    out.writeObject(contingut);
+                    out.flush();
+                    System.out.println("Fitxer enviat al client: " + ruta);
+                } else {
+                    out.writeInt(0);
+                    out.flush();
+                }
+            } catch (EOFException eof) {
+                System.out.println("Error llegint el fitxer del client: null");
+                break;
+            } catch (ClassNotFoundException cnfe) {
+                System.err.println("Error de tipus rebut: " + cnfe.getMessage());
+            }
+        }
+        System.out.println("Nom del fitxer buit o nul. Sortint...");
+    }
     public static void main(String[] args) throws Throwable {
         Servidor serv = new Servidor();
-        Socket cliSock = serv.connectar();
+        serv.cliSocket = serv.connectar();
+        System.out.println("Connexio acceptada: " + serv.cliSocket.getInetAddress());
+        serv.out = new ObjectOutputStream(serv.cliSocket.getOutputStream());
+        serv.in = new ObjectInputStream(serv.cliSocket.getInputStream());
         serv.enviarFitxers();
-        serv.tancarConnexio(cliSock);
+        serv.tancarConnexio(serv.cliSocket);
     }
 }
